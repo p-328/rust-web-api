7e16cd3083b5286cfa1ba273ae9b86f08ee6b75c
p-328 <69773020+p-328@users.noreply.github.com>
2022-12-06 13:17:44 -0500

    ChatGPT improves code!

--- src/time_to_unix.rs
+++ src/time_to_unix.rs
@@ -2,30 +2,32 @@ use chrono::prelude::*;
 use chrono::offset::LocalResult;
 use rocket::serde::json::Json;
 use serde::Deserialize;
+use std::collections::HashMap;
 
 #[derive(Debug, PartialEq, Eq, Deserialize)]
 pub struct DtNoHours {
-	yy: i32,
-	mm: u32,
-	dd: u32
+    yy: i32,
+    mm: u32,
+    dd: u32
 }
 
 #[post("/time_unix_no_hms/<format>", format = "json", data = "<utc_time>")]
 pub fn find_unix_time(format: &str, utc_time: Json<DtNoHours>) -> String {
-	let json_data = utc_time.into_inner();
+    let json_data = utc_time.into_inner();
 
-	match Utc.with_ymd_and_hms(
-        json_data.yy, 
-        json_data.mm, 
-        json_data.dd, 0, 0, 0) {
-	    LocalResult::Single(data) => match format {
-			"millis" | "ms" => format!("{}", data.timestamp_millis()), 
-            "nanos" | "ns" => format!("{}", data.timestamp_nanos()),
-            "secs" | "s" => format!("{}", data.timestamp()),
-            "micros" | "mus" => format!("{}", data.timestamp_micros()),
-            _ => String::from("Unrecognized format!")
-		},
-		LocalResult::None => String::from("Failed to parse date from JSON fields!"),
-        LocalResult::Ambiguous(_,_) => String::from("The time you passed in could be interpreted in multiple ways. This is not implemented for this API, although it will be soon!")
-	}
+    let date = NaiveDate::from_ymd(json_data.yy, json_data.mm, json_data.dd);
+    let unix_timestamp_functions = HashMap::new()
+        .insert("millis", Utc.timestamp_millis)
+        .insert("ms", Utc.timestamp_millis)
+        .insert("nanos", Utc.timestamp_nanos)
+        .insert("ns", Utc.timestamp_nanos)
+        .insert("secs", Utc.timestamp)
+        .insert("s", Utc.timestamp)
+        .insert("micros", Utc.timestamp_micros)
+        .insert("mus", Utc.timestamp_micros);
+
+    match unix_timestamp_functions.get(format) {
+        Some(func) => format!("{}", func(date)),
+        None => String::from("Unrecognized format!")
+    }
 }
